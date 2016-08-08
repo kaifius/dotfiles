c29a541201dbc6f3dd1a78b87e85d146863a1bcc
kaifius <kai.hofius@gmail.com>
2016-08-08 14:18:29 -0700

    set up for my machine

--- docker/aliases.zsh
+++
@@ -1,8 +0,0 @@
-removeUntaggedImages() {
-  docker images -q --filter "dangling=true" | xargs docker rmi
-}
-alias dockerrmid=removeUntaggedImages
-removeStoppedContainers() {
-  docker rm $(docker ps -a -q)
-}
-alias dockerrma=removeStoppedContainers
