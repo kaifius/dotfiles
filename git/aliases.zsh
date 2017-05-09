c8ff5aa7e7ac51bf27eacb95864c90ac9ae514cc
Kai Hofius <kai.hofius@gmail.com>
2017-05-09 09:49:04 -0700

    add fetch to `gpm`, add `gks` to co staging

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,12 +10,12 @@ lg() {
   git lg $1 | less -X
 }
 
-# The rest of my fun git aliases
 alias gl="echo 'did you mean lg?'"
 alias gp='git push origin HEAD'
-alias gpm='git pull origin master --prune'
+alias gpm='git pull origin master --prune && git fetch'
 alias gps='git pull origin staging --prune'
 alias gkm='git checkout master'
+alias gks='git co staging'
 alias gkl='git co -'
 alias grm='git rebase master'
 alias gc='git commit'
