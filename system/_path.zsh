5ca091d41dc1a7202c5313ac2c6260fef094eec1
Kai Hofius <kai.hofius@gmail.com>
2019-02-05 13:58:50 -0800

    path fixes for rvm

--- system/_path.zsh
+++ system/_path.zsh
@@ -1,3 +1,5 @@
 export PATH="./bin:/usr/local/bin:/usr/local/sbin:$ZSH/bin:/opt/bin:$PATH"
 export MANPATH="/usr/local/man:/usr/local/mysql/man:/usr/local/git/man:$MANPATH"
 
+# Add RVM to PATH for scripting. Make sure this is the last PATH variable change.
+export PATH="~/.rvm/gems/ruby-2.5.3/bin:$PATH"
