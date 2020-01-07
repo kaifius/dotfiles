9369c41ddfbec4d95bad3d200e34e6f0454717d0
Kai Hofius <kai.hofius@gmail.com>
2020-01-06 18:29:35 -0800

    Cleanup: move NVM and RVM to their folders + some other stuff

---
+++ node/nvm.zsh
@@ -0,0 +1,5 @@
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm alias default 10.15.0
+nvm use default
